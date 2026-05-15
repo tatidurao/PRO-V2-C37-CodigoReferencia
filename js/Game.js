b31d45fd72bf0d7bb205add8e995e35591fc01d3
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2026-05-15 15:39:28 -0300

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -21,15 +21,7 @@ class Game {
     form = new Form();
     form.display();
 
-    car1 = createSprite(width / 2 - 50, height - 100);
-    car1.addImage("car1", car1_img);
-    car1.scale = 0.07;
-
-    car2 = createSprite(width / 2 + 100, height - 100);
-    car2.addImage("car2", car2_img);
-    car2.scale = 0.07;
-
-    cars = [car1, car2];
+    
   }
 
   handleElements() {
@@ -44,9 +36,7 @@ class Game {
     Player.getPlayersInfo();
 
     if (allPlayers !== undefined) {
-      image(track, 0, -height * 5, width, height * 6);
-
-      drawSprites();
+     
     }
   }
 }
